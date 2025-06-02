08ddadceb8a000277e3c4d653bff060da16ec300
Aliaksandra <alekspekhota@gmail.com>
2025-06-02 13:34:18 +0200

    feat: add swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as swaggerUi from 'swagger-ui-express';
+import * as YAML from 'yamljs';
+import * as path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
+
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   //add validation pipe globaly
   app.useGlobalPipes(
